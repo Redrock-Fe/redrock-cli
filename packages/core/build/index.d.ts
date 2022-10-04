59e50cc7b1c991e925750612011781b04ac176e3
Nirvana <1357711537@qq.com>
2022-10-04 19:50:04 +0800

    docs: add the chovrio into contributors

--- packages/core/build/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from '/Users/nirvana/common-use/Redrock-projects/redrock-cli/packages/core/src/index';
-export { default } from '/Users/nirvana/common-use/Redrock-projects/redrock-cli/packages/core/src/index';
